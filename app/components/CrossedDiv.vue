bb6a8a61dab88c5bf246b390a39230b569fe55f8
Hassan Qasemi <hassanak813@outlook.com>
2025-12-15 19:20:46 +0330

    chnage cross color

--- app/components/CrossedDiv.vue
+++ app/components/CrossedDiv.vue
@@ -1,28 +1,31 @@
 <script setup lang="ts">
-const { as = 'div', className } = defineProps<{
-    as?: string,
-    className: string
-}>()
+const { as = "div", className } = defineProps<{
+  as?: string;
+  className: string;
+}>();
 </script>
 
 <template>
-    <component :is="as" class="relative -mb-px -ml-px border border-default">
-        <span class="cross absolute -bottom-px -left-px size-px" />
-        <span class="cross absolute -bottom-px -right-px size-px" />
-        <span class="cross absolute -left-px -top-px size-px" />
-        <span class="cross absolute -right-px -top-px size-px" />
-        <div class="relative z-10 flex h-full flex-col justify-center" :class="className">
-            <slot />
-        </div>
-    </component>
+  <component :is="as" class="relative -mb-px -ml-px border border-default">
+    <span class="cross absolute -bottom-px -left-px size-px" />
+    <span class="cross absolute -bottom-px -right-px size-px" />
+    <span class="cross absolute -left-px -top-px size-px" />
+    <span class="cross absolute -right-px -top-px size-px" />
+    <div
+      class="relative z-10 flex h-full flex-col justify-center"
+      :class="className"
+    >
+      <slot />
+    </div>
+  </component>
 </template>
 
 <style scoped>
 @reference "../assets/css/main.css";
 
 .cross {
-    @apply before:absolute after:absolute;
-    @apply before:-top-1 before:bg-inverted before:content-[''] before:w-px before:h-[9px];
-    @apply after:-left-1 after:bg-inverted after:content-[''] after:w-[9px] after:h-px;
+  @apply before:absolute after:absolute;
+  @apply before:-top-1 before:bg-primary/60 before:content-[''] before:w-px before:h-[9px];
+  @apply after:-left-1 after:bg-primary/60 after:content-[''] after:w-[9px] after:h-px;
 }
 </style>
