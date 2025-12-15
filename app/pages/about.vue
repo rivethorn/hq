75cbe32d1bceb695117fc9de467fb874a93a1f22
Hassan Qasemi <hassanak813@outlook.com>
2025-12-15 19:50:32 +0330

    fix font family

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,33 +1,37 @@
 <template>
-    <div class="flex flex-col items-start justify-center gap-16">
-        <h1 class="text-5xl font-black">About</h1>
-        <article class="text-lg">
-            I'm Hassan Qasemi, a software engineer and developer focused on creating thoughtful digital experiences. I
-            write about
-            the intersection of design, technology, and the creative process.
-            <br />
-            <br />
-            Currently, I work on building scalable design systems and exploring new ways to make complex interfaces more
-            intuitive. My background spans both visual design and front-end engineering, giving me a unique perspective
-            on how things should look and how they actually work.
-            <br />
-            <br />
-            This blog is a space for me to share what I'm learning, thinking about, and working on. Topics range from
-            technical deep-dives to broader reflections on design philosophy and the tools we use to build the web.
-        </article>
-        <USeparator size="sm" />
-        <h4 class="text-2xl font-bold -mt-4">Let's Connect</h4>
-        <div class="flex flex-col -mt-8 gap-5">
-            <NuxtLink class="links" to="https://x.com" target="_blank">Twitter / X</NuxtLink>
-            <NuxtLink class="links" to="https://github.com/rivethorn" target="_blank">GitHub</NuxtLink>
-            <NuxtLink class="links" to="https://threads.com" target="_blank">Threads</NuxtLink>
-            <NuxtLink class="links" to="mailto:therivethorn@gmail.com" target="_blank">Email</NuxtLink>
-        </div>
+  <div class="flex flex-col items-start justify-center gap-16">
+    <h1 class="text-5xl font-black">About</h1>
+    <article class="text-lg">
+      I'm Hassan Qasemi, a software engineer and developer focused on creating
+      thoughtful digital experiences. I write about the intersection of design,
+      technology, and the creative process.
+      <br />
+      <br />
+      Currently, I work on building scalable design systems and exploring new
+      ways to make complex interfaces more intuitive. My background spans both
+      visual design and front-end engineering, giving me a unique perspective on
+      how things should look and how they actually work.
+      <br />
+      <br />
+      This blog is a space for me to share what I'm learning, thinking about,
+      and working on. Topics range from technical deep-dives to broader
+      reflections on design philosophy and the tools we use to build the web.
+    </article>
+    <USeparator size="sm" />
+    <h4 class="text-2xl font-bold -mt-4">Let's Connect</h4>
+    <div class="flex flex-col -mt-8 gap-5">
+      <NuxtLink class="links" to="https://x.com" target="_blank"
+        >Twitter / X</NuxtLink
+      >
+      <NuxtLink class="links" to="https://github.com/rivethorn" target="_blank"
+        >GitHub</NuxtLink
+      >
+      <NuxtLink class="links" to="https://threads.com" target="_blank"
+        >Threads</NuxtLink
+      >
+      <NuxtLink class="links" to="mailto:therivethorn@gmail.com" target="_blank"
+        >Email</NuxtLink
+      >
     </div>
+  </div>
 </template>
-
-<style scoped>
-.links {
-    font-family: "TT Norms Pro Mono Variable";
-}
-</style>
