cecfe9d01f453ba9a09c7fe70fd3ec087b04362f
Hassan Qasemi <hassanak813@outlook.com>
2025-12-15 19:20:56 +0330

    formatting

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -2,39 +2,39 @@
 import type { NavigationMenuItem } from "@nuxt/ui";
 
 const items = computed<NavigationMenuItem[]>(() => [
-    {
-        label: "Blog",
-        to: "/",
-    },
-    {
-        label: "About",
-        to: "/about",
-    },
-    {
-        label: "GitHub",
-        to: "https://github.com/rivethorn",
-        target: "_blank",
-    },
+  {
+    label: "Blog",
+    to: "/",
+  },
+  {
+    label: "About",
+    to: "/about",
+  },
+  {
+    label: "GitHub",
+    to: "https://github.com/rivethorn",
+    target: "_blank",
+  },
 ]);
 </script>
 
 <template>
-    <UHeader mode="drawer">
-        <template #title>
-            <div class="inline-flex gap-2.5">
-                <NuxtImg src="logo-trans.png" sizes="26" class="light:invert" />
-                <h1>Rivethorn's HQ</h1>
-            </div>
-        </template>
+  <UHeader mode="drawer">
+    <template #title>
+      <div class="inline-flex gap-2.5">
+        <NuxtImg src="logo-trans.png" sizes="26" class="light:invert" />
+        <h1>Rivethorn's HQ</h1>
+      </div>
+    </template>
 
-        <UNavigationMenu :items="items" variant="link" />
+    <UNavigationMenu :items="items" variant="link" />
 
-        <template #right>
-            <ColorAnimBtn />
-        </template>
+    <template #right>
+      <ColorAnimBtn />
+    </template>
 
-        <template #body>
-            <UNavigationMenu :items="items" orientation="vertical" class="mb-[20%]" />
-        </template>
-    </UHeader>
+    <template #body>
+      <UNavigationMenu :items="items" orientation="vertical" class="mb-[20%]" />
+    </template>
+  </UHeader>
 </template>
