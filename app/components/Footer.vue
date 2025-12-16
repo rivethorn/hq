70feaa9aa579c3506a0401c0f5bb22f68e292dd1
Hassan Qasemi <hassanak813@outlook.com>
2025-12-16 20:24:45 +0330

    open source

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,36 +1,17 @@
-<script setup lang="ts">
-import type { NavigationMenuItem } from "@nuxt/ui";
-
-const items: NavigationMenuItem[] = [
-    {
-        label: "Twitter",
-        to: "https://x.com",
-        target: "_blank",
-    },
-    {
-        label: "GitHub",
-        to: "https://github.com/rivethorn",
-        target: "_blank",
-    },
-    {
-        label: "RSS",
-        to: "https://google.com",
-        target: "_blank",
-    },
-];
-</script>
-
 <template>
     <USeparator />
     <UFooter>
         <template #left>
             <p class="text-muted text-sm">
-                © {{ new Date().getFullYear() }} Hassan Qasemi. All rights reserved.
+                {{ new Date().getFullYear() }} Hassan Qasemi.
             </p>
         </template>
 
         <template #right>
-            <UNavigationMenu :items="items" variant="link" />
+            <span class=" text-xs text-center text-muted leading-relaxed">
+                This website is fully open-source, you can find the source
+                code on<NuxtLink to="https://github.com/rivethorn/hq" class="underline mx-1">GitHub</NuxtLink>
+            </span>
         </template>
     </UFooter>
 </template>
