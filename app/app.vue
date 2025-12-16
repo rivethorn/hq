82af9e11aadb83a7a43aa775a2e539dba7299ebb
Hassan Qasemi <hassanak813@outlook.com>
2025-12-16 21:18:46 +0330

    add noise effect

--- app/app.vue
+++ app/app.vue
@@ -3,6 +3,9 @@
     <Header />
 
     <UMain>
+      <div class="pointer-events-none fixed inset-0 z-40 size-full overflow-hidden">
+        <div class="pointer-events-none absolute inset-[-200%] z-50 size-[400%] bg-[url('/noise.png')] opacity-4" />
+      </div>
       <NuxtRouteAnnouncer />
       <NuxtPage class="max-w-[92%] lg:w-5xl mx-auto my-20" />
     </UMain>
