e1e3d8bfd06cb533f3de36a0c0015f8bac77e591
Hassan Qasemi <hassanak813@outlook.com>
2025-12-16 22:12:55 +0330

    cleaner header

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -5,23 +5,29 @@ const items = computed<NavigationMenuItem[]>(() => [
   {
     label: "Blog",
     to: "/",
+    class: "p-3",
   },
   {
     label: "About",
     to: "/about",
+    class: "p-3",
   },
   {
     label: "Writings",
     to: "/writings",
+    class: "p-3",
   },
 ]);
 </script>
 
 <template>
-  <UHeader mode="drawer">
+  <UHeader mode="slideover">
     <template #title>
       <div class="inline-flex gap-2.5 group">
-        <NuxtImg src="logo-trans.png" class="light:invert group-hover:opacity-60 transition-opacity size-7" />
+        <NuxtImg
+          src="logo-trans.png"
+          class="light:invert group-hover:opacity-60 transition-opacity size-7"
+        />
         <h1 class="group-hover:text-muted transition-colors">Rivethorn's HQ</h1>
       </div>
     </template>
@@ -32,13 +38,19 @@ const items = computed<NavigationMenuItem[]>(() => [
       <ColorAnimBtn class="hover:cursor-pointer" />
 
       <UTooltip text="My GitHub">
-        <UButton color="neutral" variant="ghost" to="https://github.com/rivethorn" target="_blank"
-          icon="i-simple-icons-github" aria-label="GitHub" />
+        <UButton
+          color="neutral"
+          variant="ghost"
+          to="https://github.com/rivethorn"
+          target="_blank"
+          icon="lucide-github"
+          aria-label="GitHub"
+        />
       </UTooltip>
     </template>
 
     <template #body>
-      <UNavigationMenu :items="items" orientation="vertical" class="mb-[20%]" />
+      <UNavigationMenu :items="items" orientation="vertical" class="p-1" />
     </template>
   </UHeader>
 </template>
