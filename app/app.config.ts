8309c2948fe05cf0bb80555787a5b764cbaeaf30
Hassan Qasemi <hassanak813@outlook.com>
2025-12-16 21:18:30 +0330

    add some material

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,8 +1,29 @@
 export default defineAppConfig({
+  profile: {
+    email: "hassanak813@outlook.com",
+    pictureDark: "https://avatars.githubusercontent.com/u/117447673?v=4",
+    pictureLight: "https://avatars.githubusercontent.com/u/117447673?v=4",
+  },
+  seo: {
+    title: "Rivethorn's HQ",
+    description: "Software Engineer & Web Developer",
+    url: "https://rivethorn.is-a.dev",
+    lang: "en",
+  },
+  socials: {
+    github: "https://github.com/rivethorn",
+    twitter: "https://x.com",
+    spotify: "https://open.spotify.com/user/hghagha",
+  },
   ui: {
     colors: {
       primary: "orange",
       neutral: "zinc",
+    },
+    header: {
+      slots: {
+        root: "z-30",
+      },
     },
   },
 });
