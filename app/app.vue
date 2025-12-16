cea30f50d2a4a1cf0b075f960a5c1139f2489379
Hassan Qasemi <hassanak813@outlook.com>
2025-12-16 22:12:47 +0330

    dynamic noise opacity

--- app/app.vue
+++ app/app.vue
@@ -2,8 +2,10 @@
 useSeoMeta({
   title: "Rivethorn's HQ",
   ogTitle: "Rivethorn's HQ",
-  description: "This is where you can read my blogs, thoughts and essays on programming and technology.",
-  ogDescription: "This is where you can read my blogs, thoughts and essays on programming and technology.",
+  description:
+    "This is where you can read my blogs, thoughts and essays on programming and technology.",
+  ogDescription:
+    "This is where you can read my blogs, thoughts and essays on programming and technology.",
 });
 </script>
 
@@ -12,8 +14,12 @@ useSeoMeta({
     <Header />
 
     <UMain>
-      <div class="pointer-events-none fixed inset-0 z-40 size-full overflow-hidden">
-        <div class="pointer-events-none absolute inset-[-200%] z-50 size-[400%] bg-[url('/noise.png')] opacity-4" />
+      <div
+        class="pointer-events-none fixed inset-0 z-40 size-full overflow-hidden"
+      >
+        <div
+          class="pointer-events-none absolute inset-[-200%] z-50 size-[400%] bg-[url('/noise.png')] opacity-3 lg:opacity-4"
+        />
       </div>
       <NuxtRouteAnnouncer />
       <NuxtPage class="max-w-[92%] lg:w-5xl mx-auto my-20" />
