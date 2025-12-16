8b0d6859df39ac9bfe297f6fdef9766001d15821
Hassan Qasemi <hassanak813@outlook.com>
2025-12-16 15:57:02 +0330

    move to content from supabase

--- app/pages/writings/[slug].vue
+++ app/pages/writings/[slug].vue
@@ -1,48 +1,10 @@
 <script setup lang="ts">
-import type { Metadata } from "../index.vue";
-
-const supabase = useSupabaseClient();
 const route = useRoute();
-const ast = ref<any>(null);
-const meta = ref<Metadata>();
 const slug = route.params.slug as string;
-const loading = ref(true);
 
-async function loadMeta() {
-  const { data, error } = await supabase
-    .from("posts")
-    .select("title, created_at, summary, tag, time_to_read")
-    .eq("slug", slug)
-    .single();
-
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-  meta.value = data;
-}
-
-async function loadFile() {
-  const { data, error } = await supabase.storage
-    .from("blog")
-    .download(`${slug}.md`);
-
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-  const markdown = await data.text();
-
-  ast.value = await parseMarkdown(markdown);
-}
-
-onMounted(async () => {
-  await loadMeta();
-  loadFile();
-  loading.value = false;
-});
+const { data: page } = await useAsyncData(route.path, () =>
+  queryCollection("writing").path(route.path).first()
+);
 </script>
 
 <template>
@@ -59,29 +21,24 @@ onMounted(async () => {
       <UIcon name="lucide-arrow-left" class="size-4" />
       <span>Back</span>
     </UButton>
-    <TransitionGroup v-if="!loading" name="list" tag="div" class="-mt-8" appear>
+    <TransitionGroup name="list" tag="div" class="-mt-2" appear>
       <div class="flex flex-col gap-6 mt-10">
         <div class="flex gap-3 text-muted text-sm lg:text-base">
-          <span>{{ new Date(meta?.created_at || "").toDateString() }}</span>
+          <span>{{ new Date(page?.date || "").toDateString() }}</span>
           <span>·</span>
-          <span>{{ meta?.time_to_read }}</span>
+          <span>{{ page?.ttr }}</span>
           <span>·</span>
-          <span>{{ meta?.tag }}</span>
+          <span>{{ page?.tag }}</span>
         </div>
         <span class="text-4xl lg:text-5xl font-bold lg:leading-14">{{
-          meta?.title
+          page?.title
         }}</span>
-        <span class="text-muted text-xl lg:text-2xl">{{ meta?.summary }}</span>
+        <span class="text-muted text-xl lg:text-2xl">{{
+          page?.description
+        }}</span>
       </div>
-      <MDC
-        :value="ast"
-        class="prose prose-neutral dark:prose-invert animate-in"
-      />
+      <ContentRenderer :value="page!" class="prose" />
     </TransitionGroup>
-
-    <p v-else class="text-center text-4xl animate-pulse mx-auto mt-20">
-      Loading...
-    </p>
   </article>
 </template>
 
--- app/pages/writings/index.vue
+++ app/pages/writings/index.vue
@@ -1,43 +1,9 @@
 <script setup lang="ts">
-import type { createTypeReferenceDirectiveResolutionCache } from "typescript";
-import type { Metadata } from "../index.vue";
+const { data: posts, error } = await useAsyncData("posts", () =>
+  queryCollection("writing").order("date", "DESC").all()
+);
 
-const supabase = useSupabaseClient();
-
-const meta = useState<Metadata[]>("posts-meta-all", () => []);
-const loading = useState<boolean>("posts-loading-all", () => true);
-
-onMounted(async () => {
-  if (meta.value.length > 0) {
-    loading.value = false;
-    return;
-  }
-
-  const { data, error } = await supabase.from("posts").select("*");
-
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-  meta.value = data
-    .map((file: Metadata) => ({
-      title: file.title,
-      tag: file.tag,
-      created_at: file.created_at,
-      time_to_read: file.time_to_read,
-      slug: file.slug,
-      summary: file.summary,
-    }))
-    .sort((a, b) => {
-      // newest first
-      return (
-        new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
-      );
-    });
-
-  loading.value = false;
-});
+if (!posts.value || !error.value) createError({ statusCode: 404 });
 </script>
 
 <template>
@@ -46,14 +12,14 @@ onMounted(async () => {
     <h3 class="text-xl font-medium text-muted">
       A complete archive of everything I've written.
     </h3>
-    <TransitionGroup v-if="!loading" name="list" tag="div" class="" appear>
+    <TransitionGroup name="list" tag="div" class="" appear>
       <span class="text-lg font-semibold text-toned">{{
-        new Date(meta.at(0)!.created_at).getFullYear()
+        new Date(posts!.at(0)!.date).getFullYear()
       }}</span>
       <NuxtLink
-        v-for="(file, index) in meta"
-        :key="file.slug"
-        :to="`/writings/${file.slug}`"
+        v-for="(post, index) in posts"
+        :key="post.title"
+        :to="post.path"
         class="group flex flex-col gap-3.5 my-8 active:scale-[0.98] transition-transform focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-muted/50"
         :style="{ '--stagger': index }"
       >
@@ -67,25 +33,21 @@ onMounted(async () => {
             class="flex gap-3 items-center justify-between text-muted text-sm lg:text-base"
           >
             <span class="text-lg font-semibold min-w-fit text-highlighted">{{
-              file.title
+              post.title
             }}</span>
             <div class="w-full"></div>
             <span class="min-w-fit">{{
-              new Date(file.created_at).toLocaleDateString("default", {
+              new Date(post.date).toLocaleDateString("default", {
                 month: "short",
                 day: "2-digit",
               })
             }}</span>
             <span>·</span>
-            <span class="min-w-fit">{{ file.time_to_read }}</span>
+            <span class="min-w-fit">{{ post.ttr }}</span>
           </div>
         </UCard>
       </NuxtLink>
     </TransitionGroup>
-
-    <p v-else class="text-center text-4xl animate-pulse mx-auto mt-20">
-      Loading...
-    </p>
   </div>
 </template>
 
@@ -101,7 +63,7 @@ onMounted(async () => {
 
 .list-enter-active {
   @apply transition-all duration-700
-         ease-[cubic-bezier(0.22,1,0.36,1)];
+        ease-[cubic-bezier(0.22,1,0.36,1)];
   transition-delay: calc(var(--stagger) * 120ms);
 }
 
