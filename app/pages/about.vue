456222e6673ea272f2192a5fb4f997a7be133e65
Hassan Qasemi <hassanak813@outlook.com>
2025-12-17 13:30:49 +0330

    add profile pic to about

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -5,8 +5,11 @@ const { data: about } = await useAsyncData("about", () =>
 </script>
 
 <template>
-  <div class="flex flex-col items-start justify-center gap-16">
-    <h1 class="text-5xl font-black">About</h1>
+  <div class="flex flex-col items-start justify-center gap-8">
+    <div class="inline-flex items-center gap-6 -mt-4">
+      <NuxtImg src="profile.jpeg" class="rounded-full size-15 lg:size-20" />
+      <h1 class="text-5xl font-black">About Me</h1>
+    </div>
     <ContentRenderer :value="about!" class="text-lg leading-relaxed font-medium" style="
         font-family:
           'ZT Nature Variable',
@@ -14,7 +17,7 @@ const { data: about } = await useAsyncData("about", () =>
       " />
     <USeparator size="sm" />
     <h4 class="text-2xl font-bold -mt-4">Let's Connect</h4>
-    <div class="flex flex-col -mt-8 gap-5">
+    <div class="flex flex-col  gap-5">
       <NuxtLink class="links" to="https://x.com" target="_blank">Twitter / X</NuxtLink>
       <NuxtLink class="links" to="https://github.com/rivethorn" target="_blank">GitHub</NuxtLink>
       <NuxtLink class="links" to="https://threads.com" target="_blank">Threads</NuxtLink>
