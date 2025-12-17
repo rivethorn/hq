7c9c2f20bd23e98a013b8aa3700c06b4ebf94d7c
Hassan Qasemi <hassanak813@outlook.com>
2025-12-18 02:55:27 +0330

    add progress bar on slug, fix header scroll issue

--- app/app.vue
+++ app/app.vue
@@ -7,25 +7,47 @@ useSeoMeta({
   ogDescription:
     "This is where you can read my blogs, thoughts and essays on programming and technology.",
 });
+
+const progress = ref(0)
+
+onMounted(() => {
+  const onScroll = () => {
+    const scrollTop = window.scrollY
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight
+
+    progress.value = height > 0 ? scrollTop / height : 0
+  }
+
+  window.addEventListener("scroll", onScroll, { passive: true })
+  onScroll()
+
+  onBeforeUnmount(() => {
+    window.removeEventListener("scroll", onScroll)
+  })
+})
 </script>
 
 <template>
   <UApp>
     <NuxtPwaAssets />
-    <Header />
+
+    <div v-if="$route.path.startsWith('/writings/')" class="fixed top-0 left-0 right-0 h-0.5 z-40">
+      <div class="h-full origin-left bg-primary" :style="{ transform: `scaleX(${progress})` }" />
+    </div>
+
+    <div class="pointer-events-none fixed inset-0 z-50 overflow-hidden">
+      <div class="absolute inset-[-200%] size-[400%] bg-[url('/noise.png')] opacity-3 lg:opacity-4" />
+    </div>
 
     <UMain>
-      <div
-        class="pointer-events-none fixed inset-0 z-40 size-full overflow-hidden"
-      >
-        <div
-          class="pointer-events-none absolute inset-[-200%] z-50 size-[400%] bg-[url('/noise.png')] opacity-3 lg:opacity-4"
-        />
-      </div>
+      <Header />
       <NuxtRouteAnnouncer />
       <NuxtPage class="max-w-[92%] lg:w-5xl mx-auto my-20" />
+      <Footer />
     </UMain>
 
-    <Footer />
   </UApp>
+
 </template>
