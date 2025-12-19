d960f756bae22f2917657256f6ce2f1444ce690f
Hassan Qasemi <hassanak813@outlook.com>
2025-12-19 17:41:44 +0330

    add RSS route

---
+++ server/routes/rss.xml.ts
@@ -0,0 +1,28 @@
+import RSS from "rss";
+
+export default defineEventHandler(async (event) => {
+  const BASE_URL = "https://rivethorn.is-a.dev";
+
+  const feed = new RSS({
+    title: "Rivethorn's HQ",
+    description: "Thoughts on programming, systems, and technology.",
+    site_url: BASE_URL,
+    feed_url: `${BASE_URL}/rss.xml`,
+  });
+
+  const posts = await queryCollection(event, "writing")
+    .order("date", "DESC")
+    .all();
+
+  posts.forEach((post) => {
+    feed.item({
+      title: post.title ?? "-",
+      url: `${BASE_URL}${post.path}`,
+      date: post.date,
+      description: post.description,
+    });
+  });
+
+  setHeader(event, "content-type", "text/xml");
+  return feed.xml({ indent: true });
+});
