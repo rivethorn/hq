73bf936cd1008d0a74caa80b080590f2f26dd187
Hassan Qasemi <hassanak813@outlook.com>
2025-12-19 22:12:20 +0330

    remove color tab from footer, tidy up

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,15 +1,10 @@
 <template>
     <USeparator />
     <UFooter class="pb-10 pt-2">
-        <template #left>
-            <ColorThemeTab />
-        </template>
-
-        <template #right>
-            <span class=" text-xs text-center text-muted leading-relaxed">
-                This website is fully open-source, you can find the source
-                code on<NuxtLink to="https://github.com/rivethorn/hq" class="underline mx-1">GitHub</NuxtLink>
-            </span>
-        </template>
+        <span class="text-xs text-center text-muted leading-relaxed">
+            This website is fully open-source, you can find the source
+            code on<NuxtLink to="https://github.com/rivethorn/hq" class="underline mx-1" target="_blank">GitHub
+            </NuxtLink>
+        </span>
     </UFooter>
 </template>
