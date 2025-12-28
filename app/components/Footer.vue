dd92c59657f0b63372e2861d35bbf9c6d0e9d0f3
Hassan Qasemi <hassanak813@outlook.com>
2025-12-29 02:28:35 +0330

    little mask on name

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,19 +1,24 @@
 <template>
-    <USeparator />
-    <UFooter class="pb-10 lg:pb-20 pt-2">
-        <template #left>
-            <ColorThemeTab />
-        </template>
+  <USeparator />
+  <UFooter class="pb-10 lg:pb-20 pt-2">
+    <template #left>
+      <ColorThemeTab />
+    </template>
 
-        <template #right>
-            <span class=" text-xs text-center text-muted leading-relaxed">
-                This website is fully open-source, you can find the source
-                code on<NuxtLink to="https://github.com/rivethorn/hq" class="underline mx-1">GitHub</NuxtLink>
-            </span>
-        </template>
-    </UFooter>
+    <template #right>
+      <span class="text-xs text-center text-muted leading-relaxed">
+        This website is fully open-source, you can find the source code
+        on<NuxtLink to="https://github.com/rivethorn/hq" class="underline mx-1"
+          >GitHub</NuxtLink
+        >
+      </span>
+    </template>
+  </UFooter>
 
-    <div class="flex justify-center">
-        <NuxtImg src="/text.svg" class="w-[92%] lg:w-5xl light:invert" />
-    </div>
+  <div class="flex justify-center">
+    <NuxtImg
+      src="/text.svg"
+      class="w-[92%] lg:w-5xl light:invert mask-b-from-60%"
+    />
+  </div>
 </template>
