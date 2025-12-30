0dfb6e13c462e21eca87d8feb668169fc4e1e5ff
Hassan Qasemi <hassanak813@outlook.com>
2025-12-31 00:46:57 +0330

    format

--- app/pages/writings/index.vue
+++ app/pages/writings/index.vue
@@ -16,15 +16,23 @@ if (!posts.value || !error.value) createError({ statusCode: 404 });
       <span class="text-lg font-semibold text-toned">{{
         new Date(posts!.at(0)!.date).getFullYear()
       }}</span>
-      <NuxtLink v-for="(post, index) in posts" :key="post.title" :to="post.path"
+      <NuxtLink
+        v-for="(post, index) in posts"
+        :key="post.title"
+        :to="post.path"
         class="group flex flex-col gap-3.5 my-8 active:scale-[0.98] transition-transform focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-muted/50"
-        :style="{ '--stagger': index }">
+        :style="{ '--stagger': index }"
+      >
         <UCard
-          class=" h-full flex flex-col gap-3.5 transition-all duration-300 hover:shadow-xl hover:shadow-black/5 hover:bg-accented/10">
-          <div class="flex flex-col lg:flex-row gap-3 items-center justify-between text-muted text-sm lg:text-base">
-            <span class="text-lg font-semibold min-w-fit text-highlighted inline-flex w-full justify-start">{{
-              post.title }}</span>
-            <!-- <div class="w-full"></div> -->
+          class="h-full flex flex-col gap-3.5 transition-all duration-300 hover:shadow-xl hover:shadow-black/5 hover:bg-accented/10"
+        >
+          <div
+            class="flex flex-col lg:flex-row gap-3 items-center justify-between text-muted text-sm lg:text-base"
+          >
+            <span
+              class="text-lg font-semibold min-w-fit text-highlighted inline-flex w-full justify-start"
+              >{{ post.title }}</span
+            >
             <div class="inline-flex w-full justify-start lg:justify-end">
               <span class="min-w-fit">{{
                 new Date(post.date).toLocaleDateString("default", {
