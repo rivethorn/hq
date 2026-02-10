2ec012cf68fd21dca17c208827b309e5cd6aedfe
Hassan Qasemi <hassanak813@outlook.com>
2026-02-11 02:36:49 +0330

    footer name motion

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -13,9 +13,16 @@
   </UFooter>
 
   <div class="flex justify-center">
-    <NuxtImg
-      src="/text.svg"
-      class="w-[92%] lg:w-5xl light:invert mask-b-from-60%"
-    />
+    <Motion
+      :initial="{ opacity: 0, y: 30 }"
+      :in-view="{ opacity: 1, y: 0 }"
+      :transition="{ duration: 0.3 }"
+      :in-view-options="{ once: true }"
+    >
+      <NuxtImg
+        src="/text.svg"
+        class="w-[92%] lg:w-5xl light:invert mask-b-from-60%"
+      />
+    </Motion>
   </div>
 </template>
