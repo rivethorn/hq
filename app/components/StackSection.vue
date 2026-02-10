0574fee5db2a141cce5724a9aa29b15813fcd9db
Hassan Qasemi <hassanak813@outlook.com>
2026-02-10 03:52:12 +0330

    stack section component made

---
+++ app/components/StackSection.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const md = `
+\`\`\`ts [stack.config.ts]
+const hassan = {
+  frontend: ["Nuxt", "Next.js", "TypeScript"],
+  backend: ["Bun", "Node", "Python", "Go" ],
+  database: ["PostgreSQL", "Supabase"],
+  systems: ["Rust", "Linux"],
+}
+\`\`\`
+`;
+</script>
+
+<template>
+  <UContainer class="py-2">
+    <div class="max-w-3xl mx-auto space-y-12">
+      <MDC :value="md" />
+    </div>
+  </UContainer>
+</template>
