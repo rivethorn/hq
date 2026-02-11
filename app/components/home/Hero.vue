4ab4125f60379356033fee42a1512f79b41d8fcb
Hassan Qasemi <hassanak813@outlook.com>
2026-02-11 22:08:09 +0330

    add fallback (?)

--- app/components/home/Hero.vue
+++ app/components/home/Hero.vue
@@ -34,3 +34,53 @@
     </Motion>
   </UPageCard>
 </template>
+
+<style lang="css" scoped>
+@keyframes wave {
+  0% {
+    transform: rotate(0deg);
+  }
+  10% {
+    transform: rotate(14deg);
+  }
+  20% {
+    transform: rotate(-8deg);
+  }
+  30% {
+    transform: rotate(14deg);
+  }
+  40% {
+    transform: rotate(-4deg);
+  }
+  50% {
+    transform: rotate(10deg);
+  }
+  60% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(0deg);
+  }
+}
+
+.animate-wave {
+  animation: wave 2.5s infinite;
+  transform-origin: 70% 70%;
+  display: inline-block;
+}
+
+@keyframes gradient {
+  0%,
+  100% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+}
+
+.animate-gradient {
+  background-size: 200% 200%;
+  animation: gradient 5s ease infinite;
+}
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -24,64 +24,10 @@ const { data: projects } = await useAsyncData("projects-latest", () =>
 
     <HomeAbout />
 
-    <HomeProjects :projects="projects" />
+    <HomeProjects :projects="projects ?? []" />
 
-    <HomeBlogs :posts="posts" />
+    <HomeBlogs :posts="posts ?? []" />
 
     <HomeCTA />
   </div>
 </template>
-
-<style lang="css" scoped>
-@keyframes wave {
-  0% {
-    transform: rotate(0deg);
-  }
-  10% {
-    transform: rotate(14deg);
-  }
-  20% {
-    transform: rotate(-8deg);
-  }
-  30% {
-    transform: rotate(14deg);
-  }
-  40% {
-    transform: rotate(-4deg);
-  }
-  50% {
-    transform: rotate(10deg);
-  }
-  60% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(0deg);
-  }
-}
-
-.animate-wave {
-  animation: wave 2.5s infinite;
-  transform-origin: 70% 70%;
-  display: inline-block;
-}
-
-@keyframes gradient {
-  0%,
-  100% {
-    background-position: 0% 50%;
-  }
-  50% {
-    background-position: 100% 50%;
-  }
-}
-
-.animate-gradient {
-  background-size: 200% 200%;
-  animation: gradient 5s ease infinite;
-}
-
-p {
-  font-family: "IBM Plex Sans";
-}
-</style>
